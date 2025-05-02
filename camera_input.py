9614e51b59e5a15986e4bbde7765dc10fa4a69dd
Arthur Muster <arthur@must-r.com>
2025-05-02 22:44:48 +0200

    feat(camera): add resize option

--- camera_input.py
+++ camera_input.py
@@ -3,10 +3,9 @@ import serial
 import time
 
 grey_channel = False
+resize = True
 SERIAL_PORT = '/dev/ttys001'
 
-ser = serial.Serial(SERIAL_PORT, 115200)
-time.sleep(5)
 
 cap = cv2.VideoCapture(1)
 
@@ -17,6 +16,9 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
+    if resize:
+        frame = cv2.resize(frame, (64, 48))
+
     if grey_channel:
         # Convert to grayscale
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -24,8 +26,6 @@ while True:
     cv2.imshow('Camera Feed', frame)
 
     # Encode and send frame data
-    _, encoded_frame = cv2.imencode('.jpg', frame)
-    ser.write(encoded_frame.tobytes())
 
     # Exit on 'q' key press
     if cv2.waitKey(1) == ord('q'):
@@ -33,5 +33,4 @@ while True:
 
 # When everything done, release the capture
 cap.release()
-ser.close()
 cv2.destroyAllWindows()
