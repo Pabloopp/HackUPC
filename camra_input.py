c428f4c9a5454325cdda398957eed51c0618d4fd
Arthur Muster <arthur@must-r.com>
2025-05-02 22:36:22 +0200

    feat(camera): add grey canal option

--- camra_input.py
+++ camra_input.py
@@ -2,12 +2,18 @@ import cv2
 
 cap = cv2.VideoCapture(1)
 
+grey_channel = False
+
 while True:
     ret, frame = cap.read()
 
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
+
+    if grey_channel:
+        # Convert to grayscale
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('Camera Feed', frame)
 
