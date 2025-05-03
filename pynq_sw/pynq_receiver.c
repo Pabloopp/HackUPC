b46ebd1647322f222591e4def8395ca72ce1960f
Pablo <pablo.lazaro2002@gmail.com>
2025-05-03 02:21:49 +0200

    Typos

--- pynq_sw/pynq_receiver.c
+++ pynq_sw/pynq_receiver.c
@@ -1,14 +1,14 @@
 #include "xparameters.h"
 #include "xuartps.h"
 
-void init_uart(XUartPs *Uart_instance)
+void init_uart(XUartPs *uart_instance)
 {
-    XUartPs_Config *Uart_config = XUartPs_LookupConfig(XPAR_UART0_BASEADDR);
-    XUartPs_CfgInitialize(Uart_instance, Uart_config, Uart_config ->BaseAddress);
+    XUartPs_Config *uart_config = XUartPs_LookupConfig(XPAR_UART0_BASEADDR);
+    XUartPs_CfgInitialize(uart_instance, uart_config, uart_config ->BaseAddress);
     //TODO set higher baudrate? currently default config
 }
 
-void get_image(XUartPs *Uart, uint8_t *buff)
+void get_image(XUartPs *uart, uint8_t *buff)
 {
     uint8_t r_byte = 0;
     uint8_t r_byte_prev = 0;
@@ -16,14 +16,14 @@ void get_image(XUartPs *Uart, uint8_t *buff)
     while (r_byte != 0xD8 && r_byte_prev != 0xFF)
     {
         r_byte_prev = r_byte;
-        r_byte = XUartPs_RecvByte(Uart->Config.BaseAddress);
+        r_byte = XUartPs_RecvByte(uart->Config.BaseAddress);
     }
     buff[0] = 0xFF;
     buff[1] = 0xD8;
     while (r_byte != 0xD9 && r_byte_prev != 0xFF)
     {
         r_byte_prev = r_byte;
-        r_byte = XUartPs_RecvByte(Uart->Config.BaseAddress);
+        r_byte = XUartPs_RecvByte(uart->Config.BaseAddress);
         buff[i] = r_byte;
         i++;
     }
