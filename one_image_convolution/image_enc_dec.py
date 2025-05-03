eb7a27c776eefd7c952c0e8a2b0e6220f57e00e7
Arthur Muster <arthur@must-r.com>
2025-05-03 19:40:41 +0200

    feat(image): add debug prints

--- one_image_convolution/image_enc_dec.py
+++ one_image_convolution/image_enc_dec.py
@@ -12,6 +12,7 @@ ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=SERIAL_TIMEOUT)
 ser.reset_input_buffer()
 ser.reset_output_buffer()
 image = cv2.imread(INPUT_IMAGE_PATH)
+print("Image read from file: ", INPUT_IMAGE_PATH)
 
 if image is None:
     print("Error: Could not read the image.")
@@ -22,12 +23,13 @@ height, width, channels = image.shape
 b_channel, g_channel, r_channel = cv2.split(image)
 serialized_image = (height.to_bytes(4, 'big') + width.to_bytes(4, 'big')
                     + r_channel.tobytes() + g_channel.tobytes() + b_channel.tobytes())
-
+print("Image size: ", height, width, channels)
 ser.write(serialized_image)
 print("Image sent to serial port.")
 
 # Read the serialized image from the serial port
 header = ser.read(8)
+print(header)
 if len(header) < 8:
     print("Error: Incomplete header received.")
     exit()
@@ -51,6 +53,5 @@ b = np.frombuffer(data[2*num_pixels:], dtype=np.uint8).reshape((recv_height, rec
 
 received_image = cv2.merge([b, g, r])
 
-# Step 5: Save the result
 cv2.imwrite(OUTPUT_IMAGE_PATH, received_image)
 print("Received image saved to ", OUTPUT_IMAGE_PATH)
