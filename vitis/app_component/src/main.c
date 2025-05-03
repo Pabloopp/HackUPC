d4571768d9ca0351076a942752f14bf5310cd1e8
Arthur Muster <arthur@must-r.com>
2025-05-03 12:54:50 +0200

    feat(jpeg): add chip jpeg encoder

--- vitis/app_component/src/main.c
+++ vitis/app_component/src/main.c
@@ -8,6 +8,9 @@
 #define NJ_INCLUDE_HEADER_ONLY
 #include "nanojpeg.c"
 
+#include "tinyjpeg.h"
+#define ENCODING_QUALITY 90
+
 int main () {
     volatile uint32_t *LED = (uint32_t *) XPAR_XGPIO_0_BASEADDR; //Debug led
     *LED = 1;
@@ -30,10 +33,19 @@ int main () {
         int height = njGetHeight();
         const uint8_t *rgb_data = njGetImage();
 
-        memcpy(tx_buffer, rx_buffer, sizeof(rx_buffer));
-        send_frame(uart0, tx_buffer, image_size);
+        // Convolution here
+
+        // Encode JPEG
+        tinyjpeg_t jpeg;
+        tinyjpeg_init(&jpeg, width, height);
+        uint8_t *encoded_data = (uint8_t *)malloc(width * height * 4);
+        size_t encoded_size = tinyjpeg_encode(&jpeg, rgb_data, encoded_data, width, height, ENCODING_QUALITY);
+
+        memcpy(tx_buffer, encoded_data, sizeof(rx_buffer));
+        send_frame(uart0, tx_buffer, encoded_size);
 
         njDone();
+        free(encoded_data);
     }
     return 1;
 }
